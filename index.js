46baca7a97bf8ae53b55c6e3d8e8240f7c2361d4
oyeniyiisaac <isaacoyeniyi6@gmail.com>
2026-04-09 09:05:58 +0100

    Add edit and delete modals with functionality for item management

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 allItem = []
+let editIndex;
+let delIndex;
 const addItem = () => {
     const itemInput = document.getElementById('itemInput').value
     const alert = document.getElementById('alert')
@@ -20,9 +22,34 @@ const addItem = () => {
 
     }
 }
+// let editInput = document.getElementById('editInput')
+const editItemInput = (i) => {
+    console.log(i);
+    editIndex = i
+    editInput.value = allItem[i]
+}
+const editItem = () => {
+    // alert('edit')
+    const editInput = document.getElementById('editInput').value
+    if (allItem.includes(editInput)) {
+        alert('your item already added')
+    } else {
+        allItem.splice(editIndex, 1, editInput)
+        showItem()
+    }
+}
+const deleteItem = (i) => {
+    console.log(i);
+    delIndex = i
+}
+
+const delItem = () => {
+    allItem.splice(delIndex, 1)
+    showItem()
+}
 function showItem() {
     showItems.innerHTML = ''
     for (i = 0; i < allItem.length; i++){
-        showItems.innerHTML += `<p>${i + 1}.${allItem[i]}</p>`
+        showItems.innerHTML += `<p class="d-flex gap-3">${i + 1}.${allItem[i]} <button class="btn btn-primary" onclick="editItemInput(${i})" data-bs-toggle="modal" data-bs-target="#exampleModal">Edit</button><button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#staticBackdrop" onclick="deleteItem(${i})">Delete</button></p>`;
     }
 }
